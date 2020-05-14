b6bced7ab664aa9542f05cb1ec96fcf7c630916f
Martin Löffler <me@fatalerrorcoded.eu>
2020-05-15 00:41:24 +0200

    Manual generation, better info

--- src/index.js
+++ src/index.js
@@ -62,49 +62,75 @@ const saveNetworkInfo = (savepath) => {
     });
 }
 
-const saveLogGenInfo = async (savepath, powershellInfo) => {
+const saveLogGenInfo = async (savepath, eventLogIndex) => {
     let info = "";
-    info += `These logs were automatically generated at ${new Date().toString()}\n`;
-    info += `Generated using https://github.com/fatalerrorcoded/Vanguard-LogGen after a vgc crash was detected`;
+    if (eventLogIndex) info += `These logs were automatically generated at ${new Date().toString()} after a vgc crash was detected\n`;
+    else info += `These logs were manually generated at ${new Date().toString()}\n`;
 
-    if (powershellInfo) {
+    info += `Generated using https://github.com/fatalerrorcoded/Vanguard-LogGen`;
+
+    if (eventLogIndex) {
+        let powershellInfo = await (() => new Promise((resolve, reject) => {
+            exec(`PowerShell.exe "Get-EventLog -LogName System -Index ${eventLogIndex} | Select-Object Data,EntryType,Message,Source,TimeGenerated"`, (error, stdout, stderr) => {
+                if (error) {
+                    reject(`Exec error: ${error}`);
+                    return;
+                }
+                resolve(stdout);
+            });
+        }))();
+
         info += "\n\n";
-        info += "The following info was reported from the Powershell command Get-EventLog\n";
+        info += "The following info was reported from the event log\n";
         info += powershellInfo;
     }
 
     await fs.writeFile(savepath, info);
+    console.log("Saved loggen info");
 }
 
-const saveLogs = (saveDirectory, powershellInfo) => {
+const saveLogs = (saveDirectory, eventLogIndex) => {
     console.log(`Saving logs to ${saveDirectory}`);
     return Promise.all([
         saveDxdiag(path.join(saveDirectory, "Riot dxdiag.txt")),
         saveProcesses(path.join(saveDirectory, "Riot Process.txt")),
         saveNetworkInfo(path.join(saveDirectory, "Riot NetworkInfo.txt")),
-        saveLogGenInfo(path.join(saveDirectory, "LogGen Info.txt"), powershellInfo)
+        saveLogGenInfo(path.join(saveDirectory, "LogGen Info.txt"), eventLogIndex)
     ]);
 }
 
-setInterval(() => {
-    exec('PowerShell.exe Get-EventLog -Newest 1 -LogName "System"', (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Exec error: ${error}`);
-            return;
-        }
-        let secondLine = stdout.split("\n")[3];
-        let index = secondLine.substring(3);
-        index = index.substring(0, index.indexOf(" "));
-        if (indexSaved !== index) {
-            if (secondLine.indexOf("vgc") !== -1 && indexSaved !== undefined) {
-                console.log("Vanguard crashed, collecting logs");
-
-                const logSpecificFolder = path.join(logFolder, index);
-                fs.mkdir(logSpecificFolder).then(() => saveLogs(logSpecificFolder));
-            }
-            indexSaved = index;
-        }
-        });
-}, 5000);
-
-console.log("Watching the vgc service");
+if (process.argv[2] !== undefined) {
+    switch (process.argv[2].toLowerCase()) {
+        case "/watch":
+        case "--watch":
+            setInterval(() => {
+                exec('PowerShell.exe Get-EventLog -Newest 1 -LogName System', (error, stdout, stderr) => {
+                    if (error) {
+                        console.error(`Exec error: ${error}`);
+                        return;
+                    }
+                    let secondLine = stdout.split("\n")[3];
+                    let index = secondLine.substring(3);
+                    index = index.substring(0, index.indexOf(" "));
+                    if (indexSaved !== index) {
+                        if (secondLine.indexOf("vgc") !== -1 && indexSaved !== undefined) {
+                            console.log("Vanguard crashed, collecting logs");
+        
+                            const logSpecificFolder = path.join(logFolder, index);
+                            fs.mkdir(logSpecificFolder).then(() => saveLogs(logSpecificFolder, index));
+                        }
+                        indexSaved = index;
+                    }
+                    });
+            }, 5000);
+        
+            console.log("Watching the vgc service");
+            break;
+        default:
+            console.log("Usage: node src/index.js [--watch]");
+            break;
+    }
+} else {
+    const logSpecificFolder = path.join(logFolder, "manual");
+    fs.mkdir(logSpecificFolder).then(() => saveLogs(logSpecificFolder, undefined));
+}
